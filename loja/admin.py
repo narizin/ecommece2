90a860c13b4b3b843369743941011e47314796dc
Esley <erbf@discente.ifpe.edu.br>
2023-03-14 08:54:38 -0300

    Projeto inicial

--- loja/admin.py
+++ loja/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Fornecedor, Categoria, Cliente
+from .models import Fornecedor, Categoria, Cliente, Loja, Pagamento,Cor
 
 admin.site.register(Fornecedor)
 admin.site.register(Categoria)
 admin.site.register(Cliente)
+admin.site.register(Loja)
+admin.site.register(Pagamento)
+admin.site.register(Cor)
--- loja/models.py
+++ loja/models.py
@@ -11,20 +11,21 @@ class Fornecedor(models.Model):
     telefone = models.CharField(max_length=120)
     email = models.EmailField(max_length=100)
 
-    class Cliente(models.Model):
-        nome = models.CharField(max_length=120)
-        endereco = models.CharField(max_length=120)
-        cidade = models.CharField(max_length=120)
-        estado = models.CharField(max_length=2)
-        telefone = models.CharField(max_length=120)
-        email = models.EmailField(max_length=100)
-        CPF = models.CharField(max_length=30)
-        CEP = models.CharField(max_length=11)
+class Cliente(models.Model):
+    nome = models.CharField(max_length=120)
+    endereco = models.CharField(max_length=120)
+    cidade = models.CharField(max_length=120)
+    estado = models.CharField(max_length=2)
+    telefone = models.CharField(max_length=120)
+    email = models.EmailField(max_length=100)
+    CPF = models.CharField(max_length=30)
+    CEP = models.CharField(max_length=11)
 
 
 class Loja(models.Model):
     nome = models.CharField(max_length=120)
     endereco = models.CharField(max_length=120)
+    CNPJ= models.CharField(max_length=120)
     cidade = models.CharField(max_length=120)
     estado = models.CharField(max_length=2)
     telefone = models.CharField(max_length=120)
@@ -32,4 +33,7 @@ class Loja(models.Model):
     CEP = models.CharField(max_length=11)
 
 class Pagamento(models.Model):
+    nome=models.CharField(max_length=120)
+
+class Cor(models.Model):
     nome=models.CharField(max_length=120)
