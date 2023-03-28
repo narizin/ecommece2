14ca2613e51fbd38b3154b0f3d384c11aa9396d4
Esley <erbf@discente.ifpe.edu.br>
2023-03-28 10:04:53 -0300

    Projeto inicial

--- loja/views.py
+++ loja/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from  rest_framework.renderers import TamplateHTMLRenderer
 from rest_framework.views import APIView
+
+class IndexPage(APIView):
+    renderer_classes = [TamplateHTMLRenderer]
+    template_name = 'page/index.html'
+
+    def get_(self, request):
+        return Response({})
+
+
