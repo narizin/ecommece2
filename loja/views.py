2d14b58d712b01f28a6e843323c29fbba34c60a5
Esley <erbf@discente.ifpe.edu.br>
2023-03-28 09:09:48 -0300

    Projeto inicial

--- loja/views.py
+++ loja/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.response import Response
+from  rest_framework.renderers import TamplateHTMLRenderer
+from rest_framework.views import APIView
